059495dd08ea317c755da77665e687177b178436
Rahul Kumar Pandey <rahulpandey1630@gmail.com>
2024-09-25 11:16:21 +0530

    Update module4.sol

--- module4.sol
+++ module4.sol
@@ -5,77 +5,84 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20, Ownable, ERC20Burnable 
-{
-
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) 
-    {
-        // Initialize with a list of features
-        addFeature(1, "Rocket", 250);
-        addFeature(2, "HoverBoard", 180);
-        addFeature(3, "MachineGun", 220);
-    }
-
-    function mint(address to, uint256 amount) public onlyOwner 
-    {
-        _mint(to, amount);
-    }
-
-    struct Entity 
-    {
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
+    // Struct to define features
+    struct Feature {
         string name;
         uint256 price;
     }
 
-    // Mapping to store available features
-    mapping(uint256 => Entity) public features;
-
-    // Mapping to store feature names to ensure uniqueness
-    mapping(string => bool) public featureNames;
-
-    // Mapping to keep track of which features a user has redeemed
-    mapping(address => mapping(uint256 => bool)) public userFeatures;
-
-    // Event to log feature redemptions
+    mapping(uint256 => Feature) public features;
+    mapping(string => bool) private featureNames;
+    mapping(address => mapping(uint256 => bool)) private userRedeemedFeatures;
     event FeatureRedeemed(address indexed user, uint256 featureId, string featureName);
 
-    // Add a new feature
-    function addFeature(uint256 featureId, string memory name, uint256 price) public onlyOwner 
-    {
+    event FeatureUpdated(uint256 indexed featureId, string oldName, string newName, uint256 oldPrice, uint256 newPrice);
+
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        _addFeature(1, "jetfly", 99);
+        _addFeature(2, "jetrun", 299);
+        _addFeature(3, "Jetswim", 499); // New Feature added
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _addFeature(uint256 featureId, string memory name, uint256 price) internal {
         require(bytes(features[featureId].name).length == 0, "Feature ID already exists");
         require(!featureNames[name], "Feature name already exists");
 
-        features[featureId] = Entity(name, price);
+        features[featureId] = Feature(name, price);
         featureNames[name] = true;
     }
 
-    // Redeem a feature
-    function redeemFeature(uint256 featureId) public 
-    {
-        Entity memory feature = features[featureId];
+    function addFeature(uint256 featureId, string memory name, uint256 price) external onlyOwner {
+        _addFeature(featureId, name, price);
+    }
+
+    function updateFeature(uint256 featureId, string memory newName, uint256 newPrice) external onlyOwner {
+        Feature memory feature = features[featureId];
         require(bytes(feature.name).length != 0, "Feature does not exist");
-        require(!userFeatures[msg.sender][featureId], "Feature already redeemed");
-        require(balanceOf(msg.sender) >= feature.price, "Insufficient balance");
 
-        // Burn tokens from the user
-        burn(feature.price);
+        string memory oldName = feature.name;
+        uint256 oldPrice = feature.price;
 
-        // Mark the feature as redeemed by the user
-        userFeatures[msg.sender][featureId] = true;
+        require(!featureNames[newName], "New feature name already exists");
 
-        // Emit the event
+        features[featureId] = Feature(newName, newPrice);
+        featureNames[newName] = true;
+        featureNames[oldName] = false;
+
+        emit FeatureUpdated(featureId, oldName, newName, oldPrice, newPrice);
+    }
+
+    function redeemFeature(uint256 featureId) external {
+        Feature memory feature = features[featureId];
+        require(bytes(feature.name).length != 0, "Feature does not exist");
+        require(!userRedeemedFeatures[msg.sender][featureId], "Feature already redeemed");
+        require(balanceOf(msg.sender) >= feature.price, "Insufficient balance to redeem");
+        _burn(msg.sender, feature.price);
+        userRedeemedFeatures[msg.sender][featureId] = true;
+
         emit FeatureRedeemed(msg.sender, featureId, feature.name);
     }
-
-    // Explicitly added the burn function
-    function burn(uint256 amount) public override {
-        _burn(_msgSender(), amount);
+    function hasRedeemedFeature(address user, uint256 featureId) external view returns (bool) {
+        return userRedeemedFeatures[user][featureId];
     }
+    function getFeatureDetails(uint256 featureId) external view returns (string memory name, uint256 price) {
+        Feature memory feature = features[featureId];
+        require(bytes(feature.name).length != 0, "Feature does not exist");
+        return (feature.name, feature.price);
+    }
+    function removeFeature(uint256 featureId) external onlyOwner {
+        require(bytes(features[featureId].name).length != 0, "Feature does not exist");
 
-    // Check if a user has redeemed a feature
-    function hasRedeemedFeature(address user, uint256 featureId) public view returns (bool) 
-    {
-        return userFeatures[user][featureId];
+        string memory featureName = features[featureId].name;
+        delete featureNames[featureName];
+        delete features[featureId];
+    }
+    function burn(uint256 amount) public override {
+        _burn(msg.sender, amount);
     }
 }
-
